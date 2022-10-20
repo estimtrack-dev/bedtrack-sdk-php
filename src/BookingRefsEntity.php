6fa9d26bcb905eb90c2a9e85fafc0a768043e6cf
bernat-ferrer <bernatferrerm@gmail.com>
2022-10-20 11:34:43 +0200

    updated booking refs entity

--- src/BookingRefsEntity.php
+++ src/BookingRefsEntity.php
@@ -6,20 +6,41 @@ class BookingRefsEntity
 {
 
     /**
-     * @var ?array  json encoded array of bed ref and patient nhc
+     * @var ?string   json encoded array of bed ref and patient nhc
      */
-    public ?array $booking_refs = [];
+    public ?string $booking_refs = null;
 
 
-    public function addBookingRef(BookingRefEntity $entity){
+    public function getArrayBookingRefs()
+    {
 
-        $this->booking_refs[]=  json_decode(json_encode($entity),true);
+        $array = json_decode($this->booking_refs, true);
+
+        return $array ?? [];
+    }
+
+
+    public function setArrayBookingRefs(array $bookingRefs)
+    {
+
+
+        $this->booking_refs = json_encode($bookingRefs);
+
+
+    }
+
+    public function addBookingRef(BookingRefEntity $entity)
+    {
+
+        $arrayRefs = $this->getArrayBookingRefs();
+        $arrayRefs[] = json_decode(json_encode($entity), true);
+        $this->setArrayBookingRefs($arrayRefs);
 
     }
 
     public function getBookingRefs(): ?array
     {
-        return $this->booking_refs;
+        return $this->getArrayBookingRefs();
     }
 
 }
